22e8f82b9ea279ac38c9dc798be1bedaeaf58eac
edmarsht <edtoulet@gmail.com>
2022-08-24 16:33:54 +0200

    details exervises new page

--- src/page/ExerciceDetails.js
+++ src/page/ExerciceDetails.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../component/Navbar";
+import "./ExerciseDetails.css"
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 function ExerciceDetails() {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+
+      setExerciseDetail(exerciseDetailData);
+      console.log(exerciseDetailData)
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  
   return (
-    <div></div>
-  )
+    <div className="exercisedetails">
+      <h1>{exerciseDetail.name}</h1>
+    </div>
+  );
 }
 
-export default ExerciceDetails
+export default ExerciceDetails;
